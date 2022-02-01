c987970890f1f89e0179bc39b48baba70424c803
Fernando Nogueira <abert036@uottawa.ca>
2022-02-01 07:15:32 -0500

    Add supabase keys to public env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  supabase_url: 'https://xiefytjecpdmrrcvwqgx.supabase.co',
+  supabase_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY2NDAxNCwiZXhwIjoxOTU5MjQwMDE0fQ.b06Ju_ldUh83_d2FUYlfA-L91sb72nSdQk-_3o-7E_s',
 };
