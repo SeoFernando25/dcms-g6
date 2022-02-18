99ff96cc93e8237c19737bd54d0964df488d1da6
Fernando Nogueira <abert036@uottawa.ca>
2022-02-18 03:30:50 -0500

    empty gh-pages branch

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  supabase_url: 'https://xiefytjecpdmrrcvwqgx.supabase.co',
-  supabase_key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTY0MzY2NDAxNCwiZXhwIjoxOTU5MjQwMDE0fQ.b06Ju_ldUh83_d2FUYlfA-L91sb72nSdQk-_3o-7E_s',
-};
