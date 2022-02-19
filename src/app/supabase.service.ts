acdd20992a1a73993b4633f2198aa465dc59d602
Fernando Nogueira <abert036@uottawa.ca>
2022-02-19 07:41:26 -0500

    feat: update login flow

--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -1,5 +1,5 @@
 import { Injectable, OnInit, Predicate } from '@angular/core';
-import {createClient, SupabaseClient} from '@supabase/supabase-js';
+import {ApiError, createClient, PostgrestError, PostgrestResponse, Session, SupabaseClient, User} from '@supabase/supabase-js';
 import {environment} from "../environments/environment";
 
 export interface Profile {
@@ -12,18 +12,10 @@ export interface Profile {
   providedIn: 'root'
 })
 export class SupabaseService implements OnInit {
-  
-  private _isLoggedIn: boolean = false;
-  private _sessionToken: string  = localStorage.getItem('session_token') || "";
-  private supabase: SupabaseClient;
+  public _supabase: SupabaseClient;
 
   constructor() {
-    this.supabase = createClient(environment.supabase_url, environment.supabase_key);
-    let login_tok = localStorage.getItem('session_token');
-    if (login_tok) {
-      this._sessionToken = login_tok;
-      this._isLoggedIn = true;
-    }
+    this._supabase = createClient(environment.supabase_url, environment.supabase_key);
   }
 
   ngOnInit(): void {
@@ -31,61 +23,21 @@ export class SupabaseService implements OnInit {
     console.log("Supabase service initialized"); 
   }
 
-  get isLoggedIn() {
-    // Ask server if session token is valid
-    return this._isLoggedIn;
+  signUp(username: string, password: string) {
+    // return this.supabase
+    // .from('person')
+    // .insert([
+    //   { username: username, password: password },
+    // ])
+    return this._supabase.auth.signUp({email:username, password});
   }
 
-  get sessionToken() {
-    return this._sessionToken;
+  async signIn(username: string, password: string) {
+    return this._supabase.auth.signIn({email:username, password});
   }
 
-  async signIn(username: string, password: string): Promise<boolean> {
-    let r = await this.supabase
-    .rpc('login', {
-      "u_name": username,
-      "u_pass": password
-    })
-    
-    if (r.error || !r.data) {
-      console.log("Login failed:", r.error);
-      return false;
-    } else {
-      console.log("Login succeeded:", r.data);
-      this._isLoggedIn = true;
-      this._sessionToken = r.data.toString();
-      localStorage.setItem('session_token', this._sessionToken);
-      return true;
-    }
-
-   
+  signOut() {
+    return this._supabase.auth.signOut();
   }
 
-  async signOut() {
-    let r = await this.supabase.rpc('logout', {sess_token: this.sessionToken});
-    localStorage.removeItem('session_token');
-    if (r.error) {
-      console.log("Logout failed:", r.error);
-      this._isLoggedIn = false;
-      this._sessionToken = "";
-    } else {
-      console.log("Logout succeeded:", r.data);
-      this._isLoggedIn = false;
-      this._sessionToken = "";
-    }
-    return 
-  }
-
-  async usernameExists(username: string): Promise<boolean> {
-    let r = await this.supabase.from('person').select('username').eq('username', username).single();
-    if (r.error) {
-      console.log("Username check failed");
-      console.log(r.error);
-      return false;
-    } else{
-      console.log("Username exists");
-      console.log(r);
-    }
-    return true;
-  }
 }
