6af36cd820e5c2c52cb8bacb3f9501b88e875481
Fernando Nogueira <abert036@uottawa.ca>
2022-03-21 12:49:47 -0400

    add: Add a basic contact us page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AuthGuard } from './guards/auth.guard';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
@@ -8,9 +7,10 @@ import { TestimonialsComponent } from './pages/testimonials/testimonials.compone
 import { OurServicesComponent } from './pages/our-services/our-services.component';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent,  },
-  { path: 'contact', component: ContactComponent, canActivate: [AuthGuard]},
-  { path: 'testimonials', component: TestimonialsComponent},
+  { path: '', component: HomeComponent, },
+  { path: 'contact', component: ContactComponent },
+  // { path: 'some-guarded-page', component: ContactComponent, canActivate: [AuthGuard]},
+  { path: 'testimonials', component: TestimonialsComponent },
   { path: 'login', component: PageNotFoundComponent },
   { path: 'our-services', component: OurServicesComponent },
   { path: '**', component: PageNotFoundComponent },
--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { MatDialog } from '@angular/material/dialog';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { Router } from '@angular/router';
+import { SupabaseService } from 'src/app/services/supabase.service';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.scss']
 })
-export class ContactComponent implements OnInit {
+export class ContactComponent {
+  requestAppointmentForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    phone: new FormControl(''),
+    email: new FormControl(''),
+  });
 
-  constructor() { }
 
-  ngOnInit(): void {
+  constructor(
+    private snackBar: MatSnackBar,
+    private supabase: SupabaseService,
+    private dialog: MatDialog,
+    private router: Router
+  ) {
+
   }
 
+  onRequestAppointment() {
+    // TODO: Create appointments page with AuthGuard
+    this.router.navigate(['/appointments']);
+  }
 }
