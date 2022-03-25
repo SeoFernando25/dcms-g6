6d5fc9e254a39c3131bf18a003654cfddcc365a3
Henry Chen <henryc00183001@gmail.com>
2022-03-25 00:21:30 -0400

    Updated My_Review Page (Unfinish)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,6 +5,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 import { TestimonialsComponent } from './pages/testimonials/testimonials.component';
 import { OurServicesComponent } from './pages/our-services/our-services.component';
+import { MyReviewComponent } from './pages/my-review/my-review.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, },
@@ -13,6 +14,7 @@ const routes: Routes = [
   { path: 'testimonials', component: TestimonialsComponent },
   { path: 'login', component: PageNotFoundComponent },
   { path: 'our-services', component: OurServicesComponent },
+  { path: 'my-review', component: MyReviewComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
