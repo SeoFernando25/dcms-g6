8c19f5e9ce818130337c8be8fa5f79b6fb7f165b
Fernando Nogueira <abert036@uottawa.ca>
2022-03-30 13:08:10 -0400

    add: appointments list page

--- src/app/pages/patient/appointments/appointments.component.ts
+++ src/app/pages/patient/appointments/appointments.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MatSnackBar } from '@angular/material/snack-bar';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { SupabaseService } from 'src/app/services/supabase.service';
 
 @Component({
@@ -9,13 +9,35 @@ import { SupabaseService } from 'src/app/services/supabase.service';
   styleUrls: ['./appointments.component.scss'],
 })
 export class AppointmentsComponent implements OnInit {
+  displayedColumns: string[] = ['appointment_id', 'dentist_id', 'appointment_date', 'start_time', 'end_time', 'appointment_type', 'appointment_status'];
+  dataSource: any[] = [];
+
   constructor(
-    supabase: SupabaseService,
+    private supabase: SupabaseService,
     private _snackBar: MatSnackBar,
-    private router: Router
-  ) {}
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
 
   ngOnInit(): void {
-    console.log('AppointmentsComponent');
+    // Load all user appointments from database
+    let userData = this.supabase._supabase.auth.user()?.id || "err";
+    this.supabase._supabase.from('appointment').select('*').eq('patient_id', userData).then((data) => {
+      console.log(data);
+      if (data.error) {
+        this._snackBar.open(data.error.message, '', { duration: 2000 });
+      } else {
+        this.dataSource = data.body;
+      }
+    });
+  }
+
+  addAppointment() {
+
+  }
+
+  clickedRow(row: any) {
+    console.log(row);
+    this.router.navigate([row.appointment_id], { relativeTo: this.route });
   }
 }
