d377fea90259c35227043858a641a552fe2a6300
Fernando Nogueira <abert036@uottawa.ca>
2022-04-08 02:02:54 -0400

    add: filterPredicate to search component

--- src/app/pages/search/search.component.ts
+++ src/app/pages/search/search.component.ts
@@ -1,35 +1,38 @@
-import { Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
-import { timer, Subscription , Observable, map} from 'rxjs';
+import { timer, Subscription, Observable, map } from 'rxjs';
 import { SupabaseService } from 'src/app/services/supabase.service';
-import { syncBuiltinESMExports } from 'module';
-import { equal } from 'assert';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { PostgrestResponse } from '@supabase/supabase-js';
 
-export interface people {
+export interface Person {
   first_name: string;
   middle_name: string;
   last_name: string;
   gender: string;
   ssn: string;
-  date_of_birth	: string;
+  date_of_birth: string;
   phone_number: string;
-
 }
-
-const ELEMENT_DATA: people[] = [  
-];
-
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.scss']
+  styleUrls: ['./search.component.scss'],
 })
 export class SearchComponent implements OnInit {
   isChecked = false;
-  displayedColumns: string[] = ['first_name', 'middle_name', 'last_name', 'gender', 'ssn', 'date_of_birth', 'phone_number'];
-  dataSource = new MatTableDataSource<people>(ELEMENT_DATA);
+  displayedColumns: string[] = [
+    'first_name',
+    'middle_name',
+    'last_name',
+    'gender',
+    'ssn',
+    'date_of_birth',
+    'phone_number',
+  ];
+  dataSource = new MatTableDataSource<Person>([]);
 
   subscription: Subscription;
   everyThreeSeconds: Observable<number> = timer(0, 3000);
@@ -42,41 +45,56 @@ export class SearchComponent implements OnInit {
 
   @ViewChild(MatPaginator) paginator: MatPaginator;
 
-  constructor(private supabase:SupabaseService) { }
+  constructor(
+    private supabase: SupabaseService,
+    private snackBar: MatSnackBar
+  ) { }
 
   ngAfterViewInit() {
     this.dataSource.paginator = this.paginator;
   }
 
   ngOnInit(): void {
+    // Search by all user fields
+    this.dataSource.filterPredicate = (data: Person, filter: string) => {
+      const dataStr = JSON.stringify(data).toLowerCase();
+      return dataStr.indexOf(filter) != -1;
+    };
+
+    this.updateFilter();
     this.subscription = this.everyThreeSeconds.subscribe(() => {
+      this.updateFilter();
+    });
+  }
+
+  updateFilter() {
     let sb = this.supabase._supabase;
     if (this.isChecked) {
-      sb.from('employee').select('person (*)').then((data) => {
-        if(data.error){
-          console.log("data.error: ", data.error);
-        }else{
-            //console.log("data: ", data.body);
-            this.dataSource.data = data.body;
-          }
+      sb.from('employee')
+        .select('person (*)')
+        .then((data) => {
+          this.updateData(data);
         });
-    }else{
-      sb.from('patient').select('person!patient_patient_id_fkey(*)').then((data) => {
-        if(data.error){
-          console.log("data.error: ", data.error);
-        }else{
-            //console.log("data: ", data.body);
-            this.dataSource.data = data.body;
-            
-          }
+    } else {
+      sb.from('patient')
+        .select('person!patient_patient_id_fkey(*)')
+        .then((data) => {
+          this.updateData(data);
         });
     }
-    });
+  }
+
+  updateData(data: PostgrestResponse<any>) {
+    if (data.error) {
+      console.log('data.error: ', data.error);
+      this.snackBar.open('Error: ' + data.error.details, 'Close');
+    } else {
+      this.dataSource.data = data.body;
+    }
   }
 
   //Unsubscribe from the timer
   // ngOnDestroy() {
   //   this.subscription.unsubscribe();
   // }
-
 }
