09a453a11ed7d67538ddc019d4773d3f78eb5419
Fernando Nogueira <abert036@uottawa.ca>
2022-04-14 02:17:04 -0400

    add: basic billing information

--- src/app/pages/patient/billing/billing.component.ts
+++ src/app/pages/patient/billing/billing.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SupabaseService } from 'src/app/services/supabase.service';
 
 @Component({
   selector: 'app-billing',
@@ -6,7 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./billing.component.scss'],
 })
 export class BillingComponent implements OnInit {
-  constructor() {}
+  dataSource: any[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    public supabase: SupabaseService,
+  ) { }
+
+  ngOnInit(): void {
+    let sb = this.supabase._supabase;
+    sb.from('fee_charge')
+      .select(`
+        *,
+        appointment_procedure!fee_charge_procedure_id_fkey(
+          *,
+          procedure_type_id!appointment_procedure_procedure_type_id_fkey(*),
+          appointment_id!appointment_procedure_appointment_id_fkey(*)
+        )
+      `)
+      // Filter to only show the fees for the current patient
+      .eq("appointment_procedure.appointment_id.patient_id", this.supabase._supabase.auth.user()?.id || "err")
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          console.log('data.error: ', data.error);
+        }
+        else {
+          console.log(data.body);
+          this.dataSource = data.body;
+        }
+      }
+      );
+  }
 }
