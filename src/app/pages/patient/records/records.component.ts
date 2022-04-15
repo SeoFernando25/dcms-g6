8e7bad5e2041a76e560b1586816a566892536942
Fernando Nogueira <abert036@uottawa.ca>
2022-04-15 07:24:40 -0400

    add: patient records prototype

--- src/app/pages/patient/records/records.component.ts
+++ src/app/pages/patient/records/records.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { DataSource, CollectionViewer } from '@angular/cdk/collections';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { RealtimeSubscription } from '@supabase/supabase-js';
+import { SupabaseRealtimeClient } from '@supabase/supabase-js/dist/main/lib/SupabaseRealtimeClient';
+import { BehaviorSubject, Subscription, Observable, subscribeOn } from 'rxjs';
+import { SupabaseService } from 'src/app/services/supabase.service';
 
 @Component({
   selector: 'app-records',
@@ -6,10 +11,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./records.component.scss']
 })
 export class RecordsComponent implements OnInit {
+  messages: any[] = [];
 
-  constructor() { }
+  constructor(
+    private supabase: SupabaseService
+  ) { }
 
   ngOnInit(): void {
-  }
+    // If we wanted to get patient info
+    // patient_id!record_patient_id_fkey(patient_id!patient_patient_id_fkey(*)),
 
+    this.supabase._supabase.from('record')
+      .select(`*,
+        written_by!record_written_by_fkey(
+          employee_id!employee_employee_id_fkey(*)
+        ))
+      `)
+      .eq('patient_id', this.supabase._supabase.auth.user()?.id)
+      .then((data) => {
+        console.log(data);
+        if (data.error) {
+          console.log('data.error: ', data.error);
+        } else {
+          console.log(data.body);
+          this.messages = data.body;
+        }
+      }
+      );
+  }
 }
