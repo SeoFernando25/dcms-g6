0d7b3c66e25a1651d1f9b47f7fd1f15c77534b71
Henry Chen <henryc00183001@gmail.com>
2022-04-15 16:14:25 -0400

    Updated Add procedure btn

--- src/app/pages/search-appointment-detail/search-appointment-detail.component.ts
+++ src/app/pages/search-appointment-detail/search-appointment-detail.component.ts
@@ -24,6 +24,7 @@ export interface procedure {
 })
 export class SearchAppointmentDetailComponent implements OnInit {
   isDentist = false;
+  isChecked = false;
   displayedColumns: string[] = [
     'procedure_id',
     'procedure_type_id',
@@ -99,6 +100,7 @@ export class SearchAppointmentDetailComponent implements OnInit {
         this.updateData(data);
       });
 
+
       this.checkDentist();
       
   }
@@ -149,7 +151,7 @@ export class SearchAppointmentDetailComponent implements OnInit {
           console.log('Error: ', data.error);
         } else {
           if (data.body?.at(0).role_type == 'Dentist') {
-            console.log('Dentist Data:' , data);
+            //console.log('Dentist Data:' , data);
             this.isDentist = true;
           }
         }
@@ -157,8 +159,9 @@ export class SearchAppointmentDetailComponent implements OnInit {
   }
 
 
-  addProcedure(row: any) {
+
+  addProcedure() {
     //console.log(row);
-    this.dialog.open(ProcedureComponent, { data: row });
+    this.dialog.open(ProcedureComponent, { data: this.editData });
   }
 }
